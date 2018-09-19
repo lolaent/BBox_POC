bbdf055052ac2d4e3256652f922d3e6cdc2b9d98
viorelbr <viorel.turtoi@lola.tech>
2018-09-19 10:20:43 +0300

    small refactoring on message receiver

--- springkafka/src/main/java/com/lolatech/springkafka/springkafka/MessageReceiver.java
+++ springkafka/src/main/java/com/lolatech/springkafka/springkafka/MessageReceiver.java
@@ -1,8 +1,6 @@
 package com.lolatech.springkafka.springkafka;
 
 import com.lolatech.springkafka.adapter.RedisAdapter;
-import com.lolatech.springkafka.dto.TimelineDto;
-import com.lolatech.springkafka.util.GenericDeserializer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.redisson.api.RQueue;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +9,6 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import com.lolatech.springkafka.service.ElasticsearchService;
 import com.lolatech.springkafka.service.RedisQueueService;
-import com.lolatech.springkafka.util.ObjectTypesEnum;
-import com.lolatech.springkafka.util.QueueProcessor;
-
-import java.sql.Time;
 
 import javax.annotation.PostConstruct;
 
@@ -43,15 +37,5 @@ public class MessageReceiver {
 	public void receive(String content) {
 		RQueue<String> queue = redisAdapter.getQueue("SYSLOG");
 		queue.add(content);
-	}
-	
-	/*public void receive(TimelineDto timelineDto) {
-	  service.save(timelineDto);
-	}*/
-	
-	private String getTopicName(String topic){
-//		String[] topics = topic.split("-");
-//
-		return topic.replace("-", "_").toUpperCase();
 	}
 }
