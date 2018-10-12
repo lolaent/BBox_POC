b84a8603fe7caa2a65cce6bbf88d1bd73e3cc1bb
Florin Besleaga <florin.besleaga@lola.tech>
2018-10-12 11:05:28 +0300

    Openshift stuff.

---
+++ springkafka/.s2iconfig/vars.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# We need to move settings.xml inside the Docker Build image, do avoid the hardcoding path below (Docker HOME and Jnlp HOME differ)
+export MVN_BUILD_COMMAND=${MVN_BUILD_COMMAND:-"mvn -e clean install"}
