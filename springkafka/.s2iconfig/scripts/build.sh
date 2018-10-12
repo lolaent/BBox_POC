862d8a6883891a857657d96a5021fb3ce9a785b0
Florin Besleaga <florin.besleaga@lola.tech>
2018-10-12 11:47:14 +0300

    Changed packing method.

--- springkafka/.s2iconfig/scripts/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mkdir ${APP_HOME}/web && mv ${APP_HOME}/target ${APP_HOME}/web/
--- springkafka/.s2iconfig/vars.sh
+++ springkafka/.s2iconfig/vars.sh
@@ -2,3 +2,4 @@
 
 # We need to move settings.xml inside the Docker Build image, do avoid the hardcoding path below (Docker HOME and Jnlp HOME differ)
 export MVN_BUILD_COMMAND=${MVN_BUILD_COMMAND:-"mvn -e clean install"}
+export ARTIFACT_LOCATION='${APP_HOME}/target/*.jar'
