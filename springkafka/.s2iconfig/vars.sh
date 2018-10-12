00f7f5da6adc2ef1049b55b27ad1fcb96967ffc2
Florin Besleaga <florin.besleaga@lola.tech>
2018-10-12 13:09:11 +0300

    Cleanup on run.

--- springkafka/.s2iconfig/vars.sh
+++ springkafka/.s2iconfig/vars.sh
@@ -2,15 +2,13 @@
 
 # We need to move settings.xml inside the Docker Build image, do avoid the hardcoding path below (Docker HOME and Jnlp HOME differ)
 export MVN_BUILD_COMMAND=${MVN_BUILD_COMMAND:-"mvn -e clean install"}
-export ARTIFACT_LOCATION="${APP_HOME}/target/*.jar ${APP_HOME}/.s2i*"
+export ARTIFACT_LOCATION="target/*.jar .s2i*"
 
-export ELASTIC_CLUSTERNAME=${ELASTIC_CLUSTERNAME:-"docker-cluster"}
-export ELASTIC_HOST=${ELASTIC_HOST:-"${NAME}-es"}
-export ELASTIC_PORT=${ELASTIC_PORT:-"9200"}
-
-export ENVIRONMENT=${ENVIRONMENT:-"rackspace"} ## TBD Why?
-
+export BOOTSTRAP_SERVERS=${BOOTSTRAP_SERVERS:-"apache-kafka:9092"}
+export ELASTICSEARCH_CLUSTERNAME=${ELASTICSEARCH_CLUSTERNAME:-"docker-cluster"}
+export ELASTICSEARCH_HOST=${ELASTICSEARCH_HOST:-"test-es"}
+export ELASTICSEARCH_PORT=${ELASTICSEARCH_PORT:-"9300"}
+export ENVIRONMENT=${ENVIRONMENT:-"remote"}
 export REDIS_ADDRESS=${REDIS_ADDRESS:-"${NAME}-rdsdb.${DEPLOY_NAMESPACE}.svc.cluster.local:6379"}
 export REDIS_TIMEOUT=${REDIS_TIMEOUT:-"3000"}
 export REDIS_PASSWORD=${REDIS_PASSWORD:-"thisisapass"}
-
